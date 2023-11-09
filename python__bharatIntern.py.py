ed92d07df6e76475b772093be4ae1906e5ab0d09
kashhh04 <149872891+kashhh04@users.noreply.github.com>
2023-11-10 00:36:58 +0530

    Add files via upload

---
+++ python__bharatIntern.py.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the wine dataset (replace 'wine_data.csv' with your dataset)
+filepath='D:\python\inputfile\wine.csv'
+data = pd.read_csv(filepath)
+
+# Assuming the 'quality' column is the target variable, and other columns are features
+X = data.drop('quality', axis=1)
+y = data['quality']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a Linear Regression model and fit it to the training data
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model's performance
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R-squared: {r2}')
